05a4734be169c5d19753c111f00bc72376bbf0e9
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2026-04-06 15:34:48 +0200

    Update init-database.sql

--- scripts/init-database.sql
+++ scripts/init-database.sql
@@ -1,1 +1,17 @@
 
+-- create database 'DataWarehouse'
+
+USE master;
+
+create DATABASE DataWarehouse;
+
+use DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
+
